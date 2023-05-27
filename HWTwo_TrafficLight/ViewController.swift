9426e1a56b3fc770bfc4fd4bb0c29be7037fa1f5
fattony01 <a.kondratenko.dev@gmail.com>
2023-05-27 17:32:45 +0200

    set the location of all objects, set the starting transparency and rounding

--- HWTwo_TrafficLight/ViewController.swift
+++ HWTwo_TrafficLight/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        redLight.layer.cornerRadius = 70
+        yellowLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 70
+        greenLight.alpha = 0.3
+        greenLight.layer.cornerRadius = 70
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+    }
+    
 }
 
